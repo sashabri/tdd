4857ba1c7bb8582fc3a49736549c752a108fbf56
Sasha Kom <xandria97@mail.ru>
2023-05-30 00:10:50 +0300

    Stub and tests for add

--- src/main/java/phone_book/PhoneBook.java
+++ src/main/java/phone_book/PhoneBook.java
@@ -1,4 +1,9 @@
 package phone_book;
 
+import java.util.Map;
+
 public class PhoneBook {
+    public int add(String name, int number, Map<String, Integer> map) {
+        return 0;
+    }
 }
--- src/test/java/phone_book_ktest/PhoneBookTest.java
+++ src/test/java/phone_book_ktest/PhoneBookTest.java
@@ -1,4 +1,29 @@
 package phone_book_ktest;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import phone_book.PhoneBook;
+
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        Map<String, Integer> result = new TreeMap<>();
+
+        PhoneBook phoneBook = new PhoneBook();
+
+        phoneBook.add("Anya", 123569, result);
+
+        Assertions.assertEquals(result.keySet().size(), 1);
+
+        phoneBook.add("Alis", 456879, result);
+
+        Assertions.assertEquals(result.keySet().size(), 2);
+
+        phoneBook.add("Alis", 354682, result);
+
+        Assertions.assertEquals(result.keySet().size(), 2);
+    }
 }
