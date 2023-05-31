73d6697cc823f839010c14f7f096610e81430b48
Sasha Kom <xandria97@mail.ru>
2023-05-31 14:27:58 +0300

    add implemented

--- src/main/java/phone_book/PhoneBook.java
+++ src/main/java/phone_book/PhoneBook.java
@@ -1,9 +1,17 @@
 package phone_book;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class PhoneBook {
+
     public int add(String name, int number, Map<String, Integer> map) {
-        return 0;
+
+        if (!map.containsKey(name)) {
+            map.put(name, number);
+        }
+
+        return map.size();
     }
 }
