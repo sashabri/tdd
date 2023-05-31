b063c4e19ed9c8f935f6b0ffa7a624439f998467
Sasha Kom <xandria97@mail.ru>
2023-05-31 14:37:12 +0300

    add implemented исправлено с учётом реализации следующих методов

--- src/main/java/phone_book/PhoneBook.java
+++ src/main/java/phone_book/PhoneBook.java
@@ -6,10 +6,15 @@ import java.util.TreeMap;
 
 public class PhoneBook {
 
+    private Map<String, Integer> forPrintName = new TreeMap<>();
+    private Map<Integer,String> forFindName = new HashMap<>();
+
     public int add(String name, int number, Map<String, Integer> map) {
 
         if (!map.containsKey(name)) {
             map.put(name, number);
+            forPrintName.put(name,number);
+            forFindName.put(number, name);
         }
 
         return map.size();
